b3dcf575c6d34e90ea268311c74540192027df1c
Yeidy Lopera <yeidylopera9@gmail.com>
2026-02-15 15:05:20 -0500

    Opp concepts

---
+++ OPPConcepts/OPPConcepts.Frondend/Program.cs
@@ -0,0 +1,18 @@
+﻿using OPPConcepts.Backend;
+
+try
+{
+    var date1 = new Date();              //constructor sin parametros
+    var date2 = new Date(2026, 2, 28);   // constructor con parametros
+    var date3 = new Date(2012, 11, 30);
+
+    Console.WriteLine(date1);
+    Console.WriteLine(date2);
+    Console.WriteLine(date3);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
---
+++ OPPConcepts/OPPConcepts/Class1.cs
@@ -0,0 +1,89 @@
+﻿using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+
+namespace OPPConcepts.Backend;
+
+public class Date
+{
+    private int _year;
+    private int _month;  
+    private int _day;
+
+    public Date()    //constructor sin parametros
+    {
+        _year = 1900;
+        _month = 1;
+        _day = 1;   
+    }
+
+    public Date(int year, int month, int day)   //constructor con párametros
+    {
+        Year = year;
+        Month = month;
+        Day = day;
+    }
+
+    public int Year 
+    {
+        get => _year;
+        set => _year = ValidateYear(value); 
+    }
+
+    public int Month 
+    {
+        get => _month;
+        set => _month = ValidateMonth(value);
+    }
+
+    public int Day 
+    { 
+        get => _day;
+        set => _day = ValidateDay(value);
+    }
+
+    public override string ToString()
+    {
+        return $"{Year:0000}/{Month:00}/{Day:00}";
+    }
+
+    private int ValidateYear(int year)
+    {
+        if (year < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof (year), "Year must be grathesy than 0.");
+        }
+        return year;   
+    }
+
+    private int ValidateMonth(int month)
+    {
+        if (month < 1 || month > 12)
+        {
+           throw new ArgumentOutOfRangeException(nameof(month), "Month must be between 1 and 12.");  
+        }
+           return month; 
+    }
+
+    private int ValidateDay(int day)
+    {
+        if (day < 1)
+        {
+            throw new ArgumentOutOfRangeException(nameof(day), "Day must be greathesth than 0.");
+        }
+        if (day == 29 && Month == 2 && IsLeapYear(Year))
+        {
+            return day;
+        }
+        int[] daysInMonth = { 31, 28, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        if (day > daysInMonth[Month - 1])
+        {
+            throw new ArgumentOutOfRangeException(nameof(day), $"Day must be between 1 and {daysInMonth[Month - 1]} for month {Month}.");
+        }
+        return day;
+    }
+
+    private bool IsLeapYear(int year)
+    {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
