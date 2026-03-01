44bb2d3af9ab12c13e3cde9a98a8e32f83c85d5f
Yeidy Lopera <yeidylopera9@gmail.com>
2026-03-01 17:36:34 -0500

    Add hourly employee

--- OPPConcepts/OPPConcepts.Frondend/Program.cs
+++ OPPConcepts/OPPConcepts.Frondend/Program.cs
@@ -3,12 +3,27 @@ using OPPConcepts.Backend;
 
 try
 {
-   var employee1 = new SalaryEmployee(1, "Maria", "Perez", true, new Date(1999, 5, 15), new Date(2020, 1, 10), 2500000);
-   var employee2 = new SalaryEmployee(1, "Joaquin", "Gonzales", true, new Date(19-0, 3, 15), new Date(2016, 11, 10), 10395250);
+   var employees = new List<Employee>();
+   decimal payroll = 0;
+   var employee1 = new SalaryEmployee(1010, "Maria", "Perez", true, new Date(1999, 5, 15), new Date(2020, 1, 10), 2500000);
+   employees.Add(employee1);
+   var employee2 = new SalaryEmployee(2020, "Joaquin", "Gonzales", true, new Date(1980, 3, 15), new Date(2016, 11, 10), 10395250);
+   employees.Add(employee2);
+   var employee3 = new HourlyEmployee(3030, "Ana", "Lopez", true, new Date(1970, 3, 15), new Date(2015, 11, 20), 18000, 95);
+   employees.Add(employee3);
+   var employee4 = new HourlyEmployee(4040, "Kevin", "Rua", true, new Date(1970, 3, 15), new Date(2015, 11, 20), 10000, 112);
+   employees.Add(employee3);
 
-    Console.WriteLine(employee1);
-    Console.WriteLine(employee2);
 
+    foreach (var employee in employees)
+    {
+        Console.WriteLine(new string('-', 47));
+        Console.WriteLine(employee);
+        payroll += employee.GetValueToPlay();
+    }
+
+    Console.WriteLine(new string('=', 47));
+    Console.WriteLine($"Payroll..................: {payroll,20:C2}");
 }
 catch (Exception ex)
 {
---
+++ OPPConcepts/OPPConcepts/HourlyEmployee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OPPConcepts.Backend
+{
+    public class HourlyEmployee : Employee
+    {
+        //fiels
+        private decimal _hourlyValue;
+        private float _workingHours;
+
+        //constructors
+        public HourlyEmployee(int id, string firstName, string lastName, bool isActive, Date bornDate, Date? hireDate, decimal hourlyValue, float workingHours) : 
+            base(id, firstName, lastName, isActive, bornDate, hireDate)
+        {
+            HourlyValue = hourlyValue;
+            WorkingHours = workingHours;
+        }
+
+        //properties
+        public decimal HourlyValue
+        {
+            get => _hourlyValue;
+            set => _hourlyValue = ValidateHourlyValue(value);
+        }
+        public float WorkingHours 
+        {
+            get => _workingHours;
+            set => _workingHours = ValidateWorkingHours(value);
+        }
+
+        //metodos
+        public override decimal GetValueToPlay() => (decimal)WorkingHours * HourlyValue;
+
+
+
+        override public string ToString() => base.ToString() +
+            $"\n\tHourly Value.....: {HourlyValue, 20:C2}\n\t" +
+            $"Warking Hours....: {WorkingHours,20:N2}";
+
+        private decimal ValidateHourlyValue(decimal hourlyValue)
+        {
+            if (hourlyValue < 10000)
+            {
+                throw new ArgumentOutOfRangeException(nameof(hourlyValue), "Hourly value must be greater then or equal to $10,000. ");
+            }
+            return hourlyValue;
+        }
+        private float ValidateWorkingHours(float workingHours)
+        {
+            if (workingHours < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(workingHours), "working hours ust be greater then or equal to 0. ");
+            }
+            return workingHours;
+        }
+        
+    }
+}
