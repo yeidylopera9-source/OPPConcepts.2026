7a04327fc97afd0eec797251561abdc5ecd6a2b7
Yeidy Lopera <yeidylopera9@gmail.com>
2026-03-01 16:13:24 -0500

    Add employee class

--- OPPConcepts/OPPConcepts.Frondend/Program.cs
+++ OPPConcepts/OPPConcepts.Frondend/Program.cs
@@ -1,14 +1,10 @@
-﻿using OPPConcepts.Backend;
+﻿using OPPConcepts;
+using OPPConcepts.Backend;
 
 try
 {
-    var date1 = new Date();              //constructor sin parametros
-    var date2 = new Date(2026, 2, 28);   // constructor con parametros
-    var date3 = new Date(2012, 11, 30);
-
-    Console.WriteLine(date1);
-    Console.WriteLine(date2);
-    Console.WriteLine(date3);
+   var employee = new Employee(1, "jhon", "Doe", true, new Date(1999, 5, 15), new Date(2020, 1, 10));
+    Console.WriteLine(employee);    
 }
 catch (Exception ex)
 {
@@ -16,3 +12,10 @@ catch (Exception ex)
 }
 
 
+ var date1 = new Date();              //constructor sin parametros
+    var date2 = new Date(2026, 2, 28);   // constructor con parametros
+    var date3 = new Date(2012, 11, 30);
+
+    Console.WriteLine(date1);
+    Console.WriteLine(date2);
+    Console.WriteLine(date3);
---
+++ OPPConcepts/OPPConcepts/Employee.cs
@@ -0,0 +1,40 @@
+﻿using OPPConcepts.Backend;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OPPConcepts
+{
+    public abstract class Employee
+    {
+        //constructores
+        protected Employee(int id, string firstName, string lastName, bool isActive, Date bornDate, Date? hireDate)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            IsActive = isActive;
+            BornDate = bornDate;
+            HireDate = hireDate;
+        }
+
+        //propiedades
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public bool IsActive { get; set; }
+        public Date BornDate { get; set; } = null!;
+        public Date? HireDate { get; set; } = null!;
+
+        //metodos int, long, float, double, decimal: monedas
+        public override string ToString()
+        {
+            return $"{Id}\t{FirstName} {LastName}\n\t" +
+                   $"Value to pay: {GetValueToPlay()}";
+        }
+        public abstract decimal GetValueToPlay();
+
+
+    }
+}
+
